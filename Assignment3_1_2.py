7995179e2bf02c5c4c43058b7308b567263ee566
satriaenur <satrianur@outlook.com>
2017-03-31 17:53:44 +0700

    add progress 2 and words

--- Assignment3_1_2.py
+++ Assignment3_1_2.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # types: 0 = F1-Micro, 1 = F1-Macro, 2 = Simple Accuracy
 def performance_calculator(types, target, predict):
@@ -29,4 +30,47 @@ def performance_calculator(types, target, predict):
 			recall =  (confmatrix[:,0] / (confmatrix[:,0] + confmatrix[:,1])).sum()/confmatrix.shape[0]
 		return 2*(precision * recall)/(precision + recall)
 
+def visualize_data(data,decisionboundary=False):
+	figure = plt.figure()
+	list_class = np.unique(data[:,2].astype('i'))
+	colors = ['r', 'g', 'b', 'y', 'k', 'c']
+	marker = ['o', 's', '*']
+	mcounter, ccounter = 0,0
+	for i in list_class:
+		data_class = data[data[:,2] == i]
+		plt.scatter(data_class[:,0], data_class[:,1], c=colors[(ccounter) % 6], marker=marker[(mcounter) % 3])
+		ccounter += 1
+		if(ccounter % 3 == 0): mcounter += 1
+	if (decisionboundary):
+		boundary = np.array([data.max(axis=0),data.min(axis=0)])
+		plt.plot([boundary[0,0],boundary[1,0]],[boundary[0,1],boundary[1,1]])
 
+class naive_bayes:
+	def __init__(self, data, targetcol):
+		self.data = data
+		self.targetcol = targetcol
+		self.prior = {}
+		self.mean = {}
+		self.variance = {}
+		self.learning()
+		# self.predict()
+
+	def learning(self):
+		self.list_class = np.unique(data[:,self.targetcol].astype('i'))
+		dataperclass = {}
+		for i in self.list_class:
+			dataperclass[i] = data[data[:,2] == i]
+			self.prior[i] = dataperclass[i].shape[0] / (data.shape[0] + 0.0)
+			self.mean[i] = [np.mean(dataperclass[i][:,j]) for j in xrange(dataperclass[i].shape[1] - 1)]
+			self.variance[i] = [np.var(dataperclass[i][:,j],ddof=1) for j in xrange(dataperclass[i].shape[1] - 1)]
+
+	def predict(self,values):
+		predict_result = {}
+		for i in self.list_class:
+			predict_result[i] = np.log(self.prior[i]) + np.sum([np.log(1/(np.sqrt(2*np.pi*np.sqrt(self.variance[i][j])))*np.exp(((values[j]-self.mean[i][j])**2)/(2*self.variance[i][j]))) for j in xrange(values.shape[0]-1)])
+		print predict_result, max(predict_result, key=predict_result.get)
+
+data = np.genfromtxt('jain.csv',delimiter=',')
+nb = naive_bayes(data, 2)
+print data[1]
+nb.predict(data[1])
